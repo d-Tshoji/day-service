9592074608f405043741042ec04afe243ccef81d
deer_shoji <tetuta.shoji.deer@gmail.com>
2017-08-28 20:11:48 +0900

    spメニュー バーガーアイコンにて開く事が可能に

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,9 @@
-$(function(){
-  
+$(document).on('click', '#js-nav-burger', function(){
+
+  alert("実行しました。");
+  //$('.header__nav').css("display","block");
+  $('.header__nav').fadeIn();
+
+  $('#js-nav').toggleClass('active');
+
 });

e75f50a586339e59a51f70db99a61e1a1e38f089
deer_shoji <tetuta.shoji.deer@gmail.com>
2017-08-28 23:57:19 +0900

    sp/バーガーメニュー完成

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,9 +1,11 @@
-$(document).on('click', '#js-nav-burger', function(){
-
+$(document).on('click', '#js-nav-burger', function () {
   alert("実行しました。");
-  
   $('#js-nav').toggleClass('active');
-
-  $('.active').css('transform','scaleX(1)');
-
+  $('.active').css('transform', 'scaleX(1)');
+});
+$(document).on('click', function (e) {
+  if (!$(e.target).closest('#js-nav,#js-nav-burger').length) {
+    $('.active').css('transform', 'scaleX(0)');
+    $('#js-nav').removeClass('active');
+  }
 });

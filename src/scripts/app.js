5a85f4e7f707d410e4685bb196dc3b0c42fb5f99
deer_shoji <tetuta.shoji.deer@gmail.com>
2017-08-28 22:08:30 +0900

    バーガーメニュー スライドアニメーション追加

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,9 +1,9 @@
 $(document).on('click', '#js-nav-burger', function(){
 
   alert("実行しました。");
-  //$('.header__nav').css("display","block");
-  $('.header__nav').fadeIn();
-
+  
   $('#js-nav').toggleClass('active');
+
+  $('.active').css('transform','scaleX(1)');
 
 });

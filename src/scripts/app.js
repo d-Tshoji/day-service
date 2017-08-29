b31364fdd8747ed39457a70468457a6816c8a8e9
deer_shoji <tetuta.shoji.deer@gmail.com>
2017-08-29 16:22:37 +0900

    tab切り替え完成及び初期で表示されないバグ修正

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -13,3 +13,34 @@ $('.js-nav-burger').on('click', function (event) {
 $('.js-nav').on('click', function (event) {
   event.stopPropagation();
 });
+
+$(".js-tab-all").on("click", function () {
+  $('.news__contents').removeClass('contents-active');
+  $('.js-tab-reset').removeClass('tab-active');
+  $('.js-tab-all').addClass('tab-active');
+  $('.js-all').addClass('contents-active');
+});
+
+$(".js-tab-info").on("click", function () {
+  $('.news__contents').removeClass('contents-active');
+  $('.js-tab-reset').removeClass('tab-active');
+  $('.js-tab-info').addClass('tab-active');
+  $('.js-info').addClass('contents-active');
+});
+
+$(".js-tab-topic").on("click", function () {
+  $('.news__contents').removeClass('contents-active');
+  $('.js-tab-reset').removeClass('tab-active');
+  $('.js-tab-topic').addClass('tab-active');
+  $('.js-topic').addClass('contents-active');
+});
+
+
+
+//$(".js-tab-color").on("click", function () {
+//  alert("色変えるにょ")
+//  $('.js-tab').removeClass('tab-active');
+//  $(this).addClass('tab-active');
+//});
+
+
